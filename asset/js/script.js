51c992df6e8eea705b8aab0d27d01e2c9f03a337
Pat Brown <ptrckbrown94@gmail.com>
2019-11-25 16:45:37 -0600

    city search and button work

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,27 +3,40 @@
 // imput should save history
 // display City, date, icon for weather, temp, Humidity, Wind speed, UV index
 // 5 day  forcast 
-    // date
-    // temp
-    //weather icon
-    //humidity 
+// date
+// temp
+//weather icon
+//humidity 
 
-    // two different keys for different  functions
+// two different keys for different  functions
 
-    let cityName =  document.getElementById("")
+let cityNameEl = document.getElementById("cityName")
+const searchbuttonEl = document.getElementById("searchButton")
+console.log(searchbuttonEl)
+searchbuttonEl.addEventListener("click", function () {
+    event.preventDefault();
+    const cityName = cityNameEl.value
+    console.log(cityName)
+
     const yourKey = "15230fb51b42832a4b1f952cfbe1c3d1";
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + yourKey;
-   console.log(queryUrl)
+    console.log(queryUrl)
 
     axios.get(queryUrl)
-    .then(function(response){
-        // the retrieved data
-     const weatherIcon = response.data.icon;   
-     const tempature = response.data.main.temp;
-     const humidity = response.data.main.humidity;
-     const windSpeed = response.data.wind.spead;    
-     const cityName = response.data.name;
-     const longitude = resposne.data.coord.lon;
-     const latitude = response.data.coord.lat;
+        .then(function (response) {
+            // the retrieved data needed
+            const weatherIcon = response.data.icon;
+            const tempature = response.data.main.temp;
+            const humidity = response.data.main.humidity;
+            const windSpeed = response.data.wind.spead;
+            const cityName = response.data.name;
+            const longitude = response.data.coord.lon;
+            const latitude = response.data.coord.lat;
 
-    }); 
+        });
+
+
+
+});
+
+
