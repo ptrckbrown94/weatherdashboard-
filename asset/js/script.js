6f3b85c15c16f17dc41a3fa3f8cf617507c89bab
Pat Brown <ptrckbrown94@gmail.com>
2019-11-24 21:16:07 -0600

    seudo code

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+// Pseudo Code
+// get user imput City
+// imput should save history
+// display City, date, icon for weather, temp, Humidity, Wind speed, UV index
+// 5 day  forcast 
+    // date
+    // temp
+    //weather icon
+    //humidity 
+
+
+    
